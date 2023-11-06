33a6328ecbadc920276a59a46e0278769779803a
Neaz-mq <neazmorshed666@gmail.com>
2023-11-06 10:20:51 +0600

    mongodb collection added

--- index.js
+++ index.js
@@ -29,6 +29,23 @@ async function run() {
   try {
     // Connect the client to the server	(optional starting in v4.7)
     await client.connect();
+
+    const tourCollection = client.db('tourDB').collection('tours');
+
+    app.get('/tours', async (req, res) => {
+      const cursor = tourCollection.find();
+      const result = await cursor.toArray();
+      res.send(result);
+
+    })
+
+
+
+
+
+
+
+
     // Send a ping to confirm a successful connection
     await client.db("admin").command({ ping: 1 });
     console.log("Pinged your deployment. You successfully connected to MongoDB!");
