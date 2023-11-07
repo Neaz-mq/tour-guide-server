ffd30e9dd7d62b77800405e78ce903e898798b04
Neaz-mq <neazmorshed666@gmail.com>
2023-11-07 15:46:33 +0600

    take get documnet for client side view

--- index.js
+++ index.js
@@ -73,14 +73,14 @@ async function run() {
 
     })
 
-    app.get('/service/:id', async (req, res) => {
-      const id = req.params.id;
-      const query = { _id: new ObjectId(id) }
-      const result = await serviceCollection.findOne(query);
-      res.send(result);
+    // app.get('/service', async (req, res) => {
+    //   const id = req.params.id;
+    //   const query = { _id: new ObjectId(id) }
+    //   const result = await serviceCollection.findOne(query);
+    //   res.send(result);
 
 
-    })
+    // })
 
     app.post('/service', async (req, res) => {
       const newService = req.body;
